d019af056fcce852143267af286db4379cb3021e
sleepytetris <seonjae912@gmail.com>
2022-06-29 15:03:46 +0800

    Add button to turn pixel a random colour on hover
    Also add reset button and button to turn the theme back to normal

--- javascript.js
+++ javascript.js
@@ -1,26 +1,51 @@
 const container = document.querySelector('.container');
 const pixelButton = document.querySelector('.size-change');
+const normalButton = document.querySelector('.normal-mode');
 const randomButton = document.querySelector('.random-mode');
+const resetButton = document.querySelector('.reset');
 
-function applyHover(e) {
-    e.classList.add("etched");
+function applyHover(e, theme) {
+    if (theme == "black") {
+        e.style.backgroundColor = "#6b85a0";
+    }
+    if (theme == "colourful") {
+        e.style.backgroundColor = getRandomColour();
+        e.classList.remove("etched");
+    }
 }
 
-function createGrid(n) {
+function createGrid(n, theme) {
     let total = n*n;
-    let side= 640/n - 2;
+    let side = 640/n-2;
     for (let i=0; i<total; i++) {
         let square = document.createElement('div');
         container.appendChild(square);
         square.style.width = side + "px";
         square.style.height = side + "px";
         square.addEventListener('mouseenter', () => {
-            applyHover(square);
+            applyHover(square, theme);
         })
     }
 }
 
-createGrid(16);
+function getRandomColour() {
+    var letters = '0123456789ABCDEF';
+    var colour = '#';
+    for (var i = 0; i < 6; i++) {
+        colour += letters[Math.floor(Math.random() * 16)];
+    }
+    return colour;
+}
+
+function changeTheme(theme) {
+    const pixels = container.children;
+    for (let i=0; i<pixels.length; i++) {
+        let pixel = pixels[i];
+        pixel.addEventListener('mouseenter', () => {
+            applyHover(pixel, theme);
+        })
+    }
+}
 
 pixelButton.addEventListener('click', () => {
     container.textContent = "";
@@ -34,10 +59,34 @@ pixelButton.addEventListener('click', () => {
 
         if (n > 100 || n < 1) {
             alert("You must enter a number between 1 and 100!");
-            createGrid(16);
+            createGrid(16, "black");
         }
-        else if (Number(pixels) < 100) {
-            createGrid(n);
+        else if (Number(pixels) < 100 && !colour) {
+            createGrid(n, "black");
+        }
+        else if (Number(pixels) < 100 && colour) {
+            createGrid(n, "colourful");
         }
     }
 })
+
+resetButton.addEventListener('click', () => {
+    const pixels = container.children;
+    for (let i=0; i<pixels.length; i++) {
+        let pixel = pixels[i];
+        pixel.style.backgroundColor = "#d3d0db";
+    }
+})
+
+randomButton.addEventListener('click', () => {
+    changeTheme("colourful");
+    colour = true;
+})
+
+normalButton.addEventListener('click', () => {
+    changeTheme("black");
+    colour = false;
+})
+
+colour = false;
+createGrid(16, "black");
